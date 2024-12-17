183d20593eb36a95c44a835f53f42651b2e676b0
Thiago <66889266+kurumin001@users.noreply.github.com>
2024-12-17 16:54:26 -0300

    traceroute.ps1

--- PowerShell/traceroute.ps1
+++ PowerShell/traceroute.ps1
@@ -1,1 +1,15 @@
+# Define a lista de endereços IP
+$ips = @(
+    "192.168.000.000",
+    "192.168.000.001",
+    "192.168.101.100",
+    "192.168.101.200"
+)
 
+# Itera sobre cada IP e executa o traceroute
+foreach ($ip in $ips) {
+    Write-Host "Executando traceroute para $ip" -ForegroundColor Cyan
+    # Executa o comando tracert com os parâmetros desejados
+    tracert -h 15 $ip
+    Write-Host "----------------------------------------"
+}
